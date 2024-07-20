b64842de1042fe8e511ef97598eac4b64e0b61a7
Loganathan G P <logusivam26@gmail.com>
2024-07-20 11:22:36 +0000

    Jul 20, 2024, 4:52 PM

--- script.js
+++ script.js
@@ -18,5 +18,22 @@
         yMove = y / height * (move * 2) - move;
 
         span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-    }
-})
+
+        if (e.type === 'mouseleave')
+            span.style.transform = '';
+    };
+
+    const editCursor = e => {
+        const  {
+            clientX: x,
+            clientY: y 
+        } = e;
+
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px;'
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
+})();
